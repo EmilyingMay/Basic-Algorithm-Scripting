c23a2f37214aeabdc5986bb3708f03c67c98a122
Emily <86921715+EmilyingMay@users.noreply.github.com>
2021-07-10 21:15:15 -0300

    yeah, hj foi bom :)

--- Basic Algorithm Scripting/Boo who.js
+++ Basic Algorithm Scripting/Boo who.js
@@ -1,10 +1,20 @@
 //    Boo who
 // Check if a value is classified as a boolean primitive. Return true or false.
-
 // Boolean primitives are true and false.
 
 function booWho(bool) {
-  return bool;
+  let a = true;
+  let b = false;
+  if (bool === a) {
+    console.log(true);
+    return true;
+  } else if (bool === b) {
+    console.log(true);
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
 }
 
-booWho(null);
+booWho(false);
