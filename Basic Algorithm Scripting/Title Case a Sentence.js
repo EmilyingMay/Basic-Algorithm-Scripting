07e8cb979b949d96329544c282fb7b913531e775
Emily <86921715+EmilyingMay@users.noreply.github.com>
2021-07-11 11:26:59 -0300

    passei a minha hora inteira de hoje nesse challge

--- Basic Algorithm Scripting/Title Case a Sentence.js
+++ Basic Algorithm Scripting/Title Case a Sentence.js
@@ -6,9 +6,29 @@
 // should also capitalize connecting words like the and of.
 
 function titleCase(str) {
+  console.log(str);
   let arr = str.split(" ");
-  let uppercase = arr[2];
-  console.log(uppercase);
+  let c = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let letters = arr[i].split("");
+    let a = [];
+    for (let j = 0; j < letters.length; j++) {
+      if (j === 0) {
+        let capital = letters[j].toUpperCase();
+        a.unshift(capital);
+      } else {
+        let lower = letters[j].toLowerCase();
+        a.push(lower);
+      }
+    }
+    let b = a.join("");
+    c.push(b);
+  }
+
+  let d = c.join(" ");
+  console.log(d);
+  return d;
 }
 
-titleCase("I'm a little tea pot");
+titleCase("sHoRt AnD sToUt");
