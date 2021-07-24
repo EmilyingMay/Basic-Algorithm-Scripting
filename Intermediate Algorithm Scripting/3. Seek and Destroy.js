fe96f5f925a2d321dcd0b50decfa26afa2e228e4
Emily <86921715+EmilyingMay@users.noreply.github.com>
2021-07-24 03:08:32 -0300

    Consegui passar de primeira yay :)

--- Intermediate Algorithm Scripting/3. Seek and Destroy.js
+++ Intermediate Algorithm Scripting/3. Seek and Destroy.js
@@ -7,8 +7,21 @@
 
 // Note: You have to use the arguments object.
 
-function destroyer(arr) {
-  return arr;
+function destroyer(arr, ...args) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      args.findIndex((item) => {
+        return item === arr[i];
+      }) === -1
+    ) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  console.log(newArr);
+  return newArr;
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
